0a568cf227f25be489f3578817e6af207b076ac2
yin qing <qyin@yindeMacBook-Pro.local>
2018-03-29 08:12:17 +0800

    角色扮演主页

--- frontEnd/src/pages/Roleplay/index.vue
+++ frontEnd/src/pages/Roleplay/index.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
     <div>角色扮演</div>
+    <div>
+      <!-- portfolio section -->
+      <section id="works" class="works section no-padding">
+        <div class="container-fluid">
+          <div class="row no-gutter">
+            <el-row>
+              <el-col :span="8">
+                <div class="grid-content bg-purple work"> <a href="./../../images/doctor.jpeg" class="work-box"> <img src="./../../images/doctor.jpeg" alt="">
+                  <div class="overlay">
+                    <div class="overlay-caption">
+                      <h5>前台</h5>
+                      <p><i class="fa fa-search-plus fa-2x"></i></p>
+                    </div>
+                  </div>
+                </a></div>
+              </el-col>
+              <el-col :span="8">
+                <div class="grid-content bg-purple work"> <a href="./../../images/doctor.jpeg" class="work-box"> <img src="./../../images/doctor.jpeg" alt="">
+                  <div class="overlay">
+                    <div class="overlay-caption">
+                      <h5>医助</h5>
+                      <p><i class="fa fa-search-plus fa-2x"></i></p>
+                    </div>
+                  </div>
+                </a></div>
+              </el-col>
+              <el-col :span="8">
+                <div class="grid-content bg-purple-light work"> <a href="./../../images/doctor.jpeg" class="work-box"> <img src="./../../images/doctor.jpeg" alt="">
+                  <div class="overlay">
+                    <div class="overlay-caption">
+                      <h5>医师</h5>
+                      <p><i class="fa fa-search-plus fa-2x"></i></p>
+                    </div>
+                  </div>
+                </a></div>
+              </el-col>
+            </el-row>  
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 <script>
@@ -18,6 +59,7 @@ export default {
   methods: {}
 }
 </script>
-<style lang="scss">
+<style>
+@import "./../../style/main.css";
 
 </style>
